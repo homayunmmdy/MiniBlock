3000468703ba7ff27925fcee7e3de2555355a707
Homayoun <homayoun763@gmail.com>
2025-01-18 22:13:12 +0330

    improve : dirt and grass image

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const BLOCK_SIZE = 50;
-export const SOILD_CELL = "./assets/images/dirt.jpg";
+export const SOILD_CELL = "./assets/images/dirt.webp";
 export const GRASS_CELL = "./assets/images/grass.webp";
 export const GROW_GRASS_TIME = 60000 // 1 minutes
