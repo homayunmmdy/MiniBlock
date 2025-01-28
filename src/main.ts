c6cde54da00afa1585db6cae66355a5e3154e8e5
Homayoun <homayoun763@gmail.com>
2025-01-28 16:22:05 +0330

    add : inventoary box

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <div class="gridContainer" id="gridContainer"></div>
       <div class="user" id="user"></div>
     </div>
+     <div class="inventory-box">
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+    </div>
 `
 
